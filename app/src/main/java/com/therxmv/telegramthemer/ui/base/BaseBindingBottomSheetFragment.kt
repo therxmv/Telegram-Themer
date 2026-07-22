eb504d7cea66052be746fc901b036e3d3f97c4f0
therxmv <roma.kyzmuch@gmail.com>
2026-07-22 18:53:51 +0300

    [3.1.5] Update to SDK 36

--- app/src/main/java/com/therxmv/telegramthemer/ui/base/BaseBindingBottomSheetFragment.kt
+++ app/src/main/java/com/therxmv/telegramthemer/ui/base/BaseBindingBottomSheetFragment.kt
@@ -1,12 +1,15 @@
 package com.therxmv.telegramthemer.ui.base
 
 import android.app.Dialog
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.fragment.app.FragmentManager
 import androidx.viewbinding.ViewBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.therxmv.telegramthemer.ui.base.BaseBindingDialogFragment.Companion.DEFAULT_DIM
@@ -37,6 +40,11 @@ abstract class BaseBindingBottomSheetFragment<B: ViewBinding> : BottomSheetDialo
         return requireNotNull(_dialog)
     }
 
+    override fun onStart() {
+        super.onStart()
+        forceFullScreenInLandscape()
+    }
+
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
@@ -46,5 +54,21 @@ abstract class BaseBindingBottomSheetFragment<B: ViewBinding> : BottomSheetDialo
         if (manager.isStateSaved) return
 
         super.show(manager, tag)
+    }
+
+    private fun forceFullScreenInLandscape() {
+        if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            val bottomSheet = dialog.findViewById<View>(
+                com.google.android.material.R.id.design_bottom_sheet
+            ) as? FrameLayout ?: return
+
+            bottomSheet.layoutParams.height = FrameLayout.LayoutParams.MATCH_PARENT
+
+            BottomSheetBehavior.from(bottomSheet).run {
+                state = BottomSheetBehavior.STATE_EXPANDED
+                skipCollapsed = true
+                peekHeight = bottomSheet.height
+            }
+        }
     }
 }
